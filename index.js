dbf76371020957d6628de98ffed7672d632545ae
JakeAldrinnn <custunajake21@gmail.com>
2024-03-19 15:44:12 +0800

    2nd bingo

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-console.log("Hello World.");
+console.log("Hello World");
 
-function generateRandomNumber() {
-    //Return random number 0 to 10;
-    let randomNum =Math.floor(Math.random() * 15) + 1;
 
-    // if... else if .... else statement
-    //if (conditon) {block of code to execute}
-    //if ==, !=, <, > =, < =, 
-    
-    if(randomNum> 5 ){
-        console.log(`Random Number is > = to 5`);
+let B = false, 
+    I = false,
+    N = false,
+    G = false,
+    O = false;
+
+generateRandomNumber(75);
+
+
+function generateRandomNumber (max) 
+
+{ 
+    let randomNum = Math.floor(Math.random() * max) + 1;
+ 
+    if(randomNum <= 15) 
+    { B = true;
+    console.log(`Random number ${randomNum} is in B. 1 -15`);
+    }  else if (randomNum >= 16 && randomNum <= 30) 
+    { I = true;
+    console.log(`Random number ${randomNum} is in I. 16 - 30`);
+    } else if (randomNum >= 31 && randomNum <= 45) 
+    { N = true;
+    console.log(`Random number ${randomNum} is in N. 31- 45`);
+    } else if (randomNum >= 46 && randomNum <= 60) 
+    { G = true;
+    console.log(`Random number ${randomNum} is in G. 46 - 60`);
+    } else if (randomNum >= 61 && randomNum <= 75) 
+    { O = true;
+    console.log(`Random number ${randomNum} is in O. 61 - 75`);
     } else {
-        console.log(`Random Number is < = to 5`);
+      console.log(`Random Number is invalid`);
     }
-    
-    return console.log(randomNum); 
+
+    if (B == true && I == true && N == true && G == true && O == true)
+    {
+    console.log(`BINGO!!!`);
+    }
 }
